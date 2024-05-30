e33e21a620e66e064a698e8bd63000159f742daa
UBgood <dj.indiana35@gmail.com>
2024-05-30 22:33:20 +0300

    if else

--- experiment.py
+++ experiment.py
@@ -30,26 +30,21 @@ def run_experiment(m1, m2, m3, eps, N): #eps is the ideal value we try to mainta
         
     for i in range(N): 
         # epsilon greedy 
-<<<<<<< HEAD
         #p = np.random.random() #take arbitrary action let 
         p = 0.001 + (0.1 - 0.001) * np.random.random() 
         print('\np value')
         print(p)
 
         if p < eps: #takes number in range 
-=======
-        p = np.random.random() #take arbitrary action let 
-        if p < eps: #p could be a range
->>>>>>> dfbd0e36125fe0b4869ec6fcf1b44d6f900f0e49
             jtarget = np.random.choice(3) 
             x = arms[jtarget].choose() 
             # Check the condition on the chosen action `j` #Let J= deformation 
-            if (0.5 <= jtarget <= 1): #1 represents the ideal value how can I make it a range? if it is a range then it is the optimal value  
-                x = np.max([a.choose() for a in arms]) #if p is <eps choose an action b such that jtarget<=1 gets a max reward while jtarget>=1 gets min reward. I want to add this condition in the algorithm
+            #if (0.5 <= jtarget <= 1): #1 represents the ideal value how can I make it a range? if it is a range then it is the optimal value  
+                #x = np.max([a.choose() for a in arms]) #if p is <eps choose an action b such that jtarget<=1 gets a max reward while jtarget>=1 gets min reward. I want to add this condition in the algorithm
                 #print('\nif ran \n')
                 #print(x)
-            else:
-                x = np.min([a.choose() for a in arms])
+            #else:
+                #x = np.min([a.choose() for a in arms])
                 #print('\nelse ran \n')
                 #print(x)
         else: 
